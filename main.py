a80b74830da6f3d0cd2ace8a98966be3bf059e14
Kubilay ATAŞ <kubilay1233@gmail.com>
2024-06-02 19:42:45 +0300

    up

---
+++ main.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  2 17:24:22 2024
+
+@author: kubil
+"""
+
+import gi
+
+gi.require_version("Gtk","3.0")
+gi.repository import Gtk as gtk
+
+
+class Main:
+    def __init__(self):
+        gladeFile = "main.glide"
+        self.builder = gtk.Builder()
+        self.builder.add_from_file(gladeFile)
+
+
+
+
+if __name__ == "__main__":
+    main = Main()
+    gtk.main()
