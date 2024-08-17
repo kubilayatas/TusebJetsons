02fd3ec01f3d2de805ae0d1e0f8a75226f99ab97
Kubilay ATAŞ <kubilay1233@gmail.com>
2024-08-18 01:07:16 +0300

    Update test_i2c.py

--- test_i2c.py
+++ test_i2c.py
@@ -9,8 +9,9 @@ def convert_data(data):
     for i in range(0,24,2):
         sensor_reading = (data[i] << 8) + data[i+1]
         sensor_list.append(sensor_reading)
-
     return sensor_list
+
+
 while(1):
     data = bus.read_i2c_block_data(39, 0, 24, force=None)
     listt = convert_data(data)
