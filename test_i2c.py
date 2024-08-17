40ecf1e05f9aae72c0b598321f27faaca382f9d4
Kubilay ATAŞ <kubilay1233@gmail.com>
2024-08-18 01:04:25 +0300

    Update test_i2c.py

--- test_i2c.py
+++ test_i2c.py
@@ -1,27 +1,17 @@
-from PyQt5.QtCore import *
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-import time 
-import sys 
-import numpy as np
-import math
+import time
 import smbus2 as smbus
 
 bus = smbus.SMBus(1)
 time.sleep(1)
 
 def convert_data(data):
-    fsr_list = []
-    for i in range(0,7):
-        fsr_reading = (data[i] << 8) + data[i+1]
-        fsr_list.append(fsr_reading)
+    sensor_list = []
+    for i in range(0,24,2):
+        sensor_reading = (data[i] << 8) + data[i+1]
+        sensor_list.append(sensor_reading)
 
-    return fsr_list
-
-buffer = [[i*j for j in range(1, 8)] for i in range(1, 32)]
-
-for addr in range(0,34):
-    data = bus.read_i2c_block_data(addr+8, 0, 24, force=None)
-    data = convert_data(data)
-    buffer[addr] = data
-    print("adres:{addr};data:{data}\n")
+    return sensor_list
+while(1):
+    data = bus.read_i2c_block_data(39, 0, 24, force=None)
+    listt = convert_data(data)
+    print(listt)
